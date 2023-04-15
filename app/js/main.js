8ac056f3c2433eaa0c232980289798dc9f1ed5c2
EdKazik <edoskazik@gmail.com>
2023-04-15 18:28:29 +0200

    finish commit

--- app/js/main.js
+++ app/js/main.js
@@ -46,8 +46,8 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-var image = document.getElementsByClassName('thumbnail');
-new simpleParallax(image, {
-	delay: .6,
-	transition: 'cubic-bezier(0,0,0,1)'
-});
+// var image = document.getElementsByClassName('thumbnail');
+// new simpleParallax(image, {
+// 	delay: .6,
+// 	transition: 'cubic-bezier(0,0,0,1)'
+// });
